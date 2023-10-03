e09468c1d99fe1ae335bf2ea007505820aa94497
junha kim <tto9011@naver.com>
2023-10-04 00:15:24 +0900

    드가자

--- workbook/src/App.js
+++ workbook/src/App.js
@@ -1,6 +1,14 @@
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Movie from "./Components/Movie";
+import { movies } from "./movieDummy";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app-container">
+      {movies.results.map((item) => {
+        return <Movie />;
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ workbook/src/Components/Movie.jsx
@@ -0,0 +1,10 @@
+function Movie(props) {
+  return (
+    <>
+      <div>{props.Movie}</div>
+      <div>hi</div>
+    </>
+  );
+}
+
+export default Movie;
