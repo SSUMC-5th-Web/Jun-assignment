d40c65549493d4573949df83ee7664ae3e54e6a0
junha kim <tto9011@naver.com>
2023-10-30 16:06:36 +0900

    세팅

---
+++ week5/src/App.js
@@ -0,0 +1,22 @@
+import Movie from "./Components/Movie";
+import { movies } from "./movieDummy";
+import { AppContainer } from "./Components/Movie.style";
+
+function App() {
+  return (
+    <AppContainer>
+      {movies.results.map((item) => {
+        return (
+          <Movie
+            title={item.title}
+            poster_path={item.poster_path}
+            vote_average={item.vote_average}
+            overview={item.overview}
+          />
+        );
+      })}
+    </AppContainer>
+  );
+}
+
+export default App;
