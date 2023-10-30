86566f13b95e128ed11ba2cec6cfcb62b4bd7433
junha kim <tto9011@naver.com>
2023-10-31 01:01:54 +0900

    dd

--- week4/src/Components/Movie.jsx
+++ week4/src/Components/Movie.jsx
@@ -29,7 +29,7 @@ function Movie({ title, vote_average, poster_path, overview }) {
             <MovieTitle>{title}</MovieTitle>
             <div>{overview}</div>
           </Modal>
-          <MovieImg src={posterURL + poster_path} alt="포스터 사진!" />
+          <MovieImg src={posterURL + poster_path} alt="포스터 사진" />
           <MovieInfo>
             <div>{title}</div>
             <div>{vote_average}</div>
@@ -40,7 +40,7 @@ function Movie({ title, vote_average, poster_path, overview }) {
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
         >
-          <MovieImg src={posterURL + poster_path} alt="포스터 사진!" />
+          <MovieImg src={posterURL + poster_path} alt="포스터 사진" />
           <MovieInfo>
             <div>{title}</div>
             <div>{vote_average}</div>
