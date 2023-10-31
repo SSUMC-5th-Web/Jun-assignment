8c36883e1ef48008db6172a80b88cb6514f4bd64
junha kim <tto9011@naver.com>
2023-10-31 18:41:10 +0900

    dd

--- week5/src/Components/Movie.jsx
+++ week5/src/Components/Movie.jsx
@@ -11,43 +11,31 @@ const posterURL = "https://image.tmdb.org/t/p/w500/";
 
 function Movie({ title, vote_average, poster_path, overview }) {
   const [open, setOpen] = useState(false);
+
   const handleMouseOver = () => {
     setOpen(true);
   };
+
   const handleMouseOut = () => {
     setOpen(false);
   };
 
   return (
-    <>
-      {open ? (
-        <MovieContainer
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
+    <div>
+      <MovieContainer onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        {open ? (
           <Modal>
             <MovieTitle>{title}</MovieTitle>
             <div>{overview}</div>
           </Modal>
-          <MovieImg src={posterURL + poster_path} alt="포스터 사진" />
-          <MovieInfo>
-            <div>{title}</div>
-            <div>{vote_average}</div>
-          </MovieInfo>
-        </MovieContainer>
-      ) : (
-        <MovieContainer
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          <MovieImg src={posterURL + poster_path} alt="포스터 사진" />
-          <MovieInfo>
-            <div>{title}</div>
-            <div>{vote_average}</div>
-          </MovieInfo>
-        </MovieContainer>
-      )}
-    </>
+        ) : null}
+        <MovieImg src={posterURL + poster_path} alt="포스터 사진" />
+        <MovieInfo>
+          <div>{title}</div>
+          <div>{vote_average}</div>
+        </MovieInfo>
+      </MovieContainer>
+    </div>
   );
 }
 
